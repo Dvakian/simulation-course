b84d9491ee79e418d2cff14c66659445085fbefa
Dvakian <akzakhar2003@gmail.com>
2026-03-24 18:16:47 +0700

    lab04 done??

---
+++ lab04/lab04_.py
@@ -0,0 +1,82 @@
+import random
+
+
+class BasicGenerator:
+    def __init__(self, betta, m, seed):
+        self.a = betta
+        self.m = m
+        self.x = seed
+
+    def rand(self):
+        self.x = (self.a * self.x) % self.m
+        return self.x / self.m
+
+    def generate(self, n):
+        numbers = []
+        for _ in range(n):
+            numbers.append(self.rand())
+        return numbers
+
+
+def calculate(numbers):
+    n = len(numbers)
+
+    mean = sum(numbers) / n
+
+    var = 0
+    for x in numbers:
+        var += (x - mean) ** 2
+    var = var / n
+
+    return mean, var
+
+
+def main():
+    a = 16807
+    m = 2**32
+    seed = 1
+
+    N = 100_000
+
+    theor_mean = 0.5
+    theor_var = 1.0 / 12.0
+
+    # ===Базовый минимум===
+    generator = BasicGenerator(a, m, seed)
+    numbers = generator.generate(N)
+    mean, var = calculate(numbers)
+
+    # ===Встроенный генератор===
+    numbers_ = []
+
+    for i in range(N):
+        numbers_.append(random.random())
+
+    mean_, var_ = calculate(numbers_)
+
+    diff_mean = mean - theor_mean
+    diff_var = var - theor_var
+
+    diff_mean_ = mean_ - theor_mean
+    diff_var_ = var_ - theor_var
+
+    # --- Вывод ---
+    print("Базовый датчик")
+    print("Среднее:", f"{mean:.6f}", "(diff = {:+.6e})".format(diff_mean))
+    print("Дисперсия:", f"{var:.6f}", "(diff = {:+.6e})".format(diff_var))
+
+    print()
+
+    print("Встроенный генератор")
+    print("Среднее:", f"{mean_:.6f}", "(diff = {:+.6e})".format(diff_mean_))
+    print("Дисперсия:", f"{var_:.6f}", "(diff = {:+.6e})".format(diff_var_))
+
+    print()
+
+    print("Теоретические значения")
+    print("Среднее:", f"{theor_mean:.6f}")
+    print("Дисперсия:", f"{theor_var:.6f}")
+
+
+if __name__ == "__main__":
+    main()
